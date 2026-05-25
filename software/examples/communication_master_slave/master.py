0f3333824b6241992847c6de9de89d2fa493e711
michi774 <schmidtmichael376@gmail.com>
2026-05-26 00:14:33 +0200

    feat: Add WLAN Access Point setup function

--- software/examples/communication_master_slave/master.py
+++ software/examples/communication_master_slave/master.py
@@ -1,5 +1,20 @@
 from machine import Pin
 import utime
+import network
+import rp2
+
+# WLAN Access Point einrichten
+def wapCreate():
+    rp2.country('DE')
+    wap = network.WLAN(network.AP_IF)
+    wap.config(essid='Kicker-TFF', password='kicker')
+    wap.active(True)
+    netConfig = wap.ifconfig()
+    print('WLAN AP Aktiv')
+    print('Verbinde dich mit "Kicker-TFF" (PW: kicker)')
+    print('Öffne im Browser: http://' + netConfig[0])
+
+ip_adresse = wapCreate()
 
 CLK  = Pin(12, Pin.OUT)
 DATA = Pin(13, Pin.OUT)
